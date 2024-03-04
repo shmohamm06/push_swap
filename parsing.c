6bc21ddd2b1cfd0fb8ea30fe485ed97c1748bc12
Shahriar Shadman Mohammad <138768347+shmohamm06@users.noreply.github.com>
2024-03-04 23:22:42 +0400

    Update parsing.c

--- parsing.c
+++ parsing.c
@@ -6,27 +6,11 @@
 /*   By: shmohamm <shmohamm@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 17:43:48 by shmohamm          #+#    #+#             */
-/*   Updated: 2024/01/25 14:04:30 by shmohamm         ###   ########.fr       */
+/*   Updated: 2024/03/04 22:56:47 by shmohamm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-void	exit_program(t_stack *stack, char *msg)
-{
-	if (msg)
-		write(2, msg, ft_strlen(msg));
-	if (stack != NULL)
-	{
-		if (stack->stack_a != NULL)
-			free(stack->stack_a);
-		if (stack->stack_b != NULL)
-			free(stack->stack_b);
-		if (stack->join_args != NULL)
-			free(stack->join_args);
-	}
-	exit(1);
-}
 
 void	check_for_dup_or_sorted(t_stack *stack, int i)
 {
@@ -41,7 +25,7 @@ void	check_for_dup_or_sorted(t_stack *stack, int i)
 			while (j < stack->stack_a_size)
 			{
 				if (stack->stack_a[i] == stack->stack_a[j])
-					exit_program(stack, "Duplicate Found\n");
+					exit_program(stack, "Error\n");
 				j++;
 			}
 			i++;
@@ -80,31 +64,31 @@ void	validate_arguments(int argc, char **argv)
 
 void	combine_arguments(int argc, char **argv, t_stack *stack)
 {
-	char	*tmp;
-	char	*tmp2;
-	int		i;
+	char	*tmp_string1;
+	char	*tmp_string2;
+	int		index;
 
-	i = 1;
-	tmp2 = ft_strdup("");
-	while (i < argc && argv[i] != NULL)
+	index = 1;
+	tmp_string2 = ft_strdup("");
+	while (index < argc && argv[index] != NULL)
 	{
-		tmp = ft_strjoin(tmp2, argv[i]);
-		if (tmp2)
-			free(tmp2);
-		if (i != argc - 1)
+		tmp_string1 = ft_strjoin(tmp_string2, argv[index]);
+		if (tmp_string2)
+			free(tmp_string2);
+		if (index != argc - 1)
 		{
-			tmp2 = ft_strjoin(tmp, " ");
-			if (tmp)
-				free(tmp);
-			tmp = tmp2;
+			tmp_string2 = ft_strjoin(tmp_string1, " ");
+			if (tmp_string1)
+				free(tmp_string1);
+			tmp_string1 = tmp_string2;
 		}
-		i++;
+		index++;
 	}
-	stack->join_args = ft_strdup(tmp);
+	stack->join_args = ft_strdup(tmp_string1);
 	if (stack->join_args == NULL)
 		exit_program(stack, "Error\n");
-	if (tmp)
-		free(tmp);
+	if (tmp_string1)
+		free(tmp_string1);
 }
 
 int	is_stack_sorted(t_stack *stack)
