5ccb16f3e7a6663206a7016d9bb405603257c327
Shahriar Shadman Mohammad <138768347+shmohamm06@users.noreply.github.com>
2024-03-06 11:05:34 +0400

    Update utilities.c

--- utilities.c
+++ utilities.c
@@ -1,16 +1,79 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utilities.c                                        :+:      :+:    :+:   */
+/*   utilities_2.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: shmohamm <shmohamm@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 17:45:26 by shmohamm          #+#    #+#             */
-/*   Updated: 2024/03/05 10:27:55 by shmohamm         ###   ########.fr       */
+/*   Updated: 2024/03/06 11:04:13 by shmohamm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	is_stack_sorted(t_stack *stack)
+{
+	int	i;
+
+	i = 0;
+	while (i < stack->stack_a_size - 1)
+	{
+		if (stack->stack_a[i] > stack->stack_a[i + 1])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	check_for_dup_or_sorted(t_stack *stack, int i)
+{
+	int	j;
+
+	j = 0;
+	if (i == 0)
+	{
+		while (i < stack->stack_a_size)
+		{
+			j = i + 1;
+			while (j < stack->stack_a_size)
+			{
+				if (stack->stack_a[i] == stack->stack_a[j])
+					exit_program(stack, "Error\n");
+				j++;
+			}
+			i++;
+		}
+	}
+	if (is_stack_sorted(stack))
+		exit_program(stack, NULL);
+}
+
+void	create_index(t_stack *stack)
+{
+	int	i;
+	int	j;
+	int	k;
+	int	*new_a;
+
+	new_a = malloc(stack->stack_a_size * sizeof(int));
+	if (new_a == NULL)
+		exit_program(stack, "Error\n");
+	i = -1;
+	while (++i < stack->stack_a_size)
+	{
+		k = 0;
+		j = -1;
+		while (++j < stack->stack_a_size)
+			if (stack->stack_a[i] > stack->stack_a[j])
+				k++;
+		new_a[i] = k;
+	}
+	i = stack->stack_a_size;
+	while (i--)
+		stack->stack_a[i] = new_a[i];
+	free(new_a);
+}
 
 long	ft_atoi2(const char *str, t_stack *stack)
 {
@@ -38,81 +101,6 @@ long	ft_atoi2(const char *str, t_stack *stack)
 			exit_program(stack, "Error\n");
 	}
 	return ((res * sign));
-}
-
-void	read_numbers(t_stack *stack)
-{
-	char	**tmp;
-	int		i;
-	int		z;
-	long	num;
-
-	z = 0;
-	tmp = ft_split(stack->join_args, ' ');
-	i = 0;
-	while (tmp[i] != NULL && tmp[i][0] != '\0')
-	{
-		num = ft_atoi2(tmp[i], stack);
-		if (num > INT_MAX)
-			exit_program(stack, "Error\n");
-		if (i > 2)
-		{
-			exit(1);
-		}
-		stack->stack_a[z++] = num;
-		free(tmp[i]);
-		i++;
-	}
-	free(tmp);
-}
-
-void	initialize_stacks(int argc, char **argv, t_stack *stack)
-{
-	int	i;
-
-	i = 0;
-	stack->stack_a_size = 0;
-	stack->stack_b_size = 0;
-	while (--argc)
-	{
-		if (ft_wordcount(argv[i + 1], ' '))
-			stack->stack_a_size += ft_wordcount(argv[i + 1], ' ');
-		else
-			stack->stack_a_size++;
-		i++;
-	}
-	stack->stack_a = malloc(stack->stack_a_size * sizeof(int));
-	if (stack->stack_a == NULL)
-		exit_program(stack, "Error\n");
-	stack->stack_b = malloc(stack->stack_a_size * sizeof(int));
-	if (stack->stack_b == NULL)
-		exit_program(stack, "Error\n");
-}
-
-void	create_index(t_stack *stack)
-{
-	int	i;
-	int	j;
-	int	k;
-	int	*new_a;
-
-	new_a = malloc(stack->stack_a_size * sizeof(int));
-	if (new_a == NULL)
-		exit_program(stack, "Error\n");
-	i = -1;
-	while (++i < stack->stack_a_size)
-	{
-		k = 0;
-		j = -1;
-		while (++j < stack->stack_a_size)
-			if (stack->stack_a[i] > stack->stack_a[j])
-				k++;
-		new_a[i] = k;
-	}
-	i = stack->stack_a_size;
-	while (i--)
-		stack->stack_a[i] = new_a[i];
-	free(new_a);
 }
 
 void	exit_program(t_stack *stack, char *msg)
